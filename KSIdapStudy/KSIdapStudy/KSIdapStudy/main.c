fba3a4decf6ba2c42c8371c37c751aa0cd6306c4
KulikovS <sexadmiral@gmail.com>
2015-12-29 16:41:51 +0200

    start project KSIdapStudy

---
+++ KSIdapStudy/KSIdapStudy/KSIdapStudy/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  KSIdapStudy
+//
+//  Created by KulikovS on 29.12.15.
+//  Copyright © 2015 KulikovS. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
