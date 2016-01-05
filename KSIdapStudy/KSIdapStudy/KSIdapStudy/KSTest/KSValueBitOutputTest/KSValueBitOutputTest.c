71e8af19fccd68f6765523eca63f6e0740ee63cc
KulikovS <sexadmiral@gmail.com>
2016-01-06 00:12:30 +0200

    KSPrintByteValueOutputTest refactoring

--- KSIdapStudy/KSIdapStudy/KSIdapStudy/KSTest/KSValueBitOutputTest/KSValueBitOutputTest.c
+++ KSIdapStudy/KSIdapStudy/KSIdapStudy/KSTest/KSValueBitOutputTest/KSValueBitOutputTest.c
@@ -15,19 +15,14 @@ void  KSPrintByteValueOutputTest() {
     int valueInt = 500640;
     short valueShort = 50;
     char valueChar = 1;
+    
+    puts("Value short 50");
     KSPrintByteValueOutput(&valueShort, sizeof(valueShort));
+    
+    puts("Value char 1");
     KSPrintByteValueOutput(&valueChar, sizeof(valueChar));
+    
+    puts("Value int 500640");
     KSPrintByteValueOutput(&valueInt, sizeof(valueInt));
-    }
-
-
-//void KSValueBitOutputTest() {
-//    int valueInt = 500640;
-//    short valueShort = 50;
-//    
-//    puts("Value int 500640");
-//    KSByteValueOutput(&valueInt, sizeof(valueInt));
-//    
-//    puts("Value short 50");
-//    KSByteValueOutput(&valueShort, sizeof(valueShort));
+}
 
