cd52cf039253f56389f15c007209ff38f81844b2
KulikovS <sexadmiral@gmail.com>
2016-01-17 22:43:18 +0200

    new version

--- KSIdapStudy/KSIdapStudy/KSIdapStudy/main.c
+++ KSIdapStudy/KSIdapStudy/KSIdapStudy/main.c
@@ -12,24 +12,12 @@
 #include "KSValueBitOutputTest.h"
 #include "KSHumanTest.h"
 #include "KSMacro.h"
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
-//    
-//    KSTestMacroOutputType();
-//    KSTestCarStruct();
-//
-//    KSPrintByteValueOutputTest();
+
      KSHumanTest();
 
-    int *countHuman;
-    
-    int human = 10;
-    printf("Value human %d\n", human);
-    countHuman = &human;
-    *countHuman = 5;
-    
-    printf("Value numan after pointer %d\n", human);
-    printf("Value pointer %p\n", countHuman);
-    
+
     return 0;
 }
