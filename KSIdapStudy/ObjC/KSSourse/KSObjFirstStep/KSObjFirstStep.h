3a016b96f23bd26a441c8465e8a37d7c0b7daf27
KulikovS <sexadmiral@gmail.com>
2016-02-16 22:17:02 +0200

    KSIdapProject - restruct. Target ObjC - added

---
+++ KSIdapStudy/ObjC/KSSourse/KSObjFirstStep/KSObjFirstStep.h
@@ -0,0 +1,9 @@
+//
+//  KSObjFirstStep.h
+//  KSIdapStudy
+//
+//  Created by KulikovS on 16.02.16.
+//  Copyright © 2016 KulikovS. All rights reserved.
+//
+
+
---
+++ KSIdapStudy/ObjC/KSSourse/KSObjFirstStep/KSObjFirstStep.m
@@ -0,0 +1,9 @@
+//
+//  KSObjFirstStep.m
+//  KSIdapStudy
+//
+//  Created by KulikovS on 16.02.16.
+//  Copyright © 2016 KulikovS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
