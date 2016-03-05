862b1dda108d48fb2f0d3fe6fcee836475e28273
KulikovS <sexadmiral@gmail.com>
2016-03-05 13:06:18 +0200

    Tests RandomString and AlphabetCluster added

--- KSIdapStudy/TestObjC/TestObjC.m
+++ KSIdapStudy/TestObjC/TestObjC.m
@@ -12,24 +12,22 @@
 
 @interface TestObjC : XCTestCase
 @property (nonatomic, retain) KSAlphabet *alphabet;
+@property (nonatomic, retain) NSString *string;
 
 @end
 
 @implementation TestObjC
 
 - (void)setUp {
-
     [super setUp];
 
 }
 
 - (void)tearDown {
-
     [super tearDown];
 }
 
 - (void)testAlphabetsRange {
-    
     NSLog(@"Alphabet is with capital letters. The first letter of alphabet is 'A' and the last is 'Z'");
     self.alphabet = [KSAlphabet upperCaseLettersAlphabet];
     NSLog(@"%@", self.alphabet.alphabetString);
@@ -44,13 +42,6 @@
     self.alphabet = [KSAlphabet numericLettersAlphabet];
     NSLog(@"%@", self.alphabet.alphabetString);
     XCTAssert(self.alphabet.count == 10);
-    
-
-//    
-//    NSLog(@"This is random string, created with 20 symbols numericLetters Alphabet");
-//    NSLog(@"%@", randomStringWithAlphabet);
-//    XCTAssert(randomStringWithAlphabet.length == 20);
-    
 }
 
 - (void)testAlphabetString {
@@ -61,10 +52,26 @@
 }
 
 - (void)testAlphabetsArray {
-
+    NSLog(@"RandomString with alphabets.");
+    KSAlphabet *lowerCaseAlphabet = [KSAlphabet lowerCaseLettersAlphabet];
+    KSAlphabet *upperCaseAlphabet = [KSAlphabet upperCaseLettersAlphabet];
+    NSArray *strings = [NSArray arrayWithObjects:lowerCaseAlphabet, upperCaseAlphabet, nil];
+    self.alphabet = [KSAlphabet alphabetWithAlphabetsArray:strings];
+    NSLog(@"%@", self.alphabet.alphabetString);
+    XCTAssert(self.alphabet.count == 52);
 }
 
 - (void)testRandomString {
-
+    NSLog(@"RandomString with default alphabet (lowerCaseLettersAlphabet).\n String lenght is 15");
+    self.string = [NSString randomStringWithLength:15];
+    NSLog(@"%@", self.string);
+    XCTAssert(self.string.length == 15);
+    
+    NSLog(@"RandomString with alphabet.\n String lenght is default");
+    self.alphabet = [KSAlphabet alphabetWithString:@"ABCDEFGH"];
+    self.string = [NSString randomStringWithAlphabet:self.alphabet];
+    NSLog(@"%@", self.string);
+    XCTAssert(self.string.length == 10);
 }
+
 @end
