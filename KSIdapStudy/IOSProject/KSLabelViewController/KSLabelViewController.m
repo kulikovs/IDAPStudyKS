3ba0347d24c5f26ff2af71376a39fe66508da4e1
KulikovS <sexadmiral@gmail.com>
2016-04-21 19:53:40 +0300

    added **LayoutSubviews

--- KSIdapStudy/IOSProject/KSLabelViewController/KSLabelViewController.m
+++ KSIdapStudy/IOSProject/KSLabelViewController/KSLabelViewController.m
@@ -17,39 +17,52 @@
 - (void)loadView {
     [super loadView];
     
-    NSLog(@"Load view"); //загружается
+    NSLog(@"Load view");
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSLog(@"view did load"); //загружено
+    NSLog(@"view did load");
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
-    NSLog(@"View will appear"); // скоро появится
+    NSLog(@"View will appear");
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
+    NSLog(@"view will layout subviews");
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    NSLog(@"view did layout subviews");
+    
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     
-    NSLog(@"view did appear"); //появилось
+    NSLog(@"view did appear");
     
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
     
-    NSLog(@"view will disappear"); //скоро исчезнет
+    NSLog(@"view will disappear");
 }
 
 
 - (void)viewDidDisappear:(BOOL)animated {
     [super viewDidDisappear:animated];
     
-    NSLog(@"view did disappear"); //исчезло
+    NSLog(@"view did disappear");
 }
 
 - (void)didReceiveMemoryWarning {
