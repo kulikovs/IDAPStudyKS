630315e822c1cdfec4451e4cebc594fbded47b95
KulikovS <sexadmiral@gmail.com>
2016-04-28 18:38:10 +0300

    move square - first step is ready

--- KSIdapStudy/IOSProject/KSSourse/KSLabelViewController/KSLabelView/KSLabelView.m
+++ KSIdapStudy/IOSProject/KSSourse/KSLabelViewController/KSLabelView/KSLabelView.m
@@ -8,15 +8,48 @@
 
 #import "KSLabelView.h"
 
+static const CGFloat kKSAnimationDuration = 0.1;
+
 @interface KSLabelView ()
 @property (nonatomic, assign) NSUInteger squarePosition;
 
 - (KSLabelLocation)nextSquarePosition:(KSLabelLocation)position;
+- (CGRect)frameForSquarePosition:(KSLabelLocation)position;
+- (void)setSquarePosition:(NSUInteger)squarePosition animated:(BOOL)animated;
+- (void)setSquarePosition:(NSUInteger)squarePosition animated:(BOOL)animated handler:(KSLabelHandler)handler;
 
 @end
 
 @implementation KSLabelView
 
+#pragma mark -
+#pragma mark Accessors
+
+- (void)setSquarePosition:(NSUInteger)squarePosition {
+    [self setSquarePosition:squarePosition animated:NO];
+}
+
+
+- (void)setSquarePosition:(NSUInteger)squarePosition animated:(BOOL)animated {
+    if (_squarePosition != squarePosition) {
+        [self setSquarePosition:squarePosition animated:animated handler:nil];
+    }
+}
+
+- (void)setSquarePosition:(NSUInteger)squarePosition animated:(BOOL)animated handler:(KSLabelHandler)handler {
+    [UIView animateWithDuration: animated ? kKSAnimationDuration : 0
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations: ^{
+                         self.label.frame = [self frameForSquarePosition:squarePosition];
+                     } completion:^(BOOL finished) {
+                         if (handler) {
+                             handler();
+                         }
+                         
+                         [self setSquarePosition:squarePosition];
+                     }];
+}
 
 #pragma mark -
 #pragma mark Private Implementations
@@ -37,13 +70,27 @@
     }
 }
 
+- (CGRect)frameForSquarePosition:(KSLabelLocation)position {
+    switch (position) {
+        case kKSLabelUpperLeftLocation:
+            return CGRectMake(0, 100, self.label.frame.size.width, self.label.frame.size.height);
+            
+        case kKSLabelUpperRightLocation:
+            return CGRectMake(100, 200, self.label.frame.size.width, self.label.frame.size.height);;
+            
+        case kKSLabelLowerRightLocation:
+            return CGRectMake(0, 100, self.label.frame.size.width, self.label.frame.size.height);;
+            
+        default:
+            return CGRectMake(0, 100, self.label.frame.size.width, self.label.frame.size.height);;
+    }
+}
+
 #pragma mark -
 #pragma mark Public Implementations
 
 - (void)moveLabelWithAnimated:(BOOL)animated {
-
+    self.squarePosition = [self nextSquarePosition:self.squarePosition];
 }
-
-
 
 @end
