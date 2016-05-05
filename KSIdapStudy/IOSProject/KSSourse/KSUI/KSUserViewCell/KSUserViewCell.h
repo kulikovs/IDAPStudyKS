2b60b6c46ba01ff413211bd6a3ed42b2dcf33aac
KulikovS <sexadmiral@gmail.com>
2016-05-05 21:30:51 +0300

    property for stringsLabel and stringsImage were added

--- KSIdapStudy/IOSProject/KSSourse/KSUI/KSUserViewCell/KSUserViewCell.h
+++ KSIdapStudy/IOSProject/KSSourse/KSUI/KSUserViewCell/KSUserViewCell.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface KSUserViewCell : UITableViewCell
+@property (nonatomic, strong) IBOutlet UILabel      *stringsLabel;
+@property (nonatomic, strong) IBOutlet UIImageView  *stringsImage;
 
 @end
