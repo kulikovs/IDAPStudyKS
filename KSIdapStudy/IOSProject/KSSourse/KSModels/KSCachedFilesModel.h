1edcc837fd45672cbd93789d3c4cd50be36048ab
KulikovS <sexadmiral@gmail.com>
2016-06-02 00:51:23 +0300

    KSCachedFilesModel was added

---
+++ KSIdapStudy/IOSProject/KSSourse/KSModels/KSCachedFilesModel.h
@@ -0,0 +1,13 @@
+//
+//  KSCachedFilesModel.h
+//  KSIdapStudy
+//
+//  Created by KulikovS on 01.06.16.
+//  Copyright © 2016 KulikovS. All rights reserved.
+//
+
+#import "KSModel.h"
+
+@interface KSCachedFilesModel : KSModel
+
+@end
---
+++ KSIdapStudy/IOSProject/KSSourse/KSModels/KSCachedFilesModel/KSCachedFilesModel.m
@@ -0,0 +1,13 @@
+//
+//  KSCachedFilesModel.m
+//  KSIdapStudy
+//
+//  Created by KulikovS on 01.06.16.
+//  Copyright © 2016 KulikovS. All rights reserved.
+//
+
+#import "KSCachedFilesModel.h"
+
+@implementation KSCachedFilesModel
+
+@end
